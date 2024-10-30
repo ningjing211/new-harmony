c440bf5ef70002c4691fbcb3cf16d73a761ecceb
Flow <guanjun211@gmail.com>
2024-10-30 16:52:23 +0800

    tt

--- src/js/index.js
+++ src/js/index.js
@@ -366,9 +366,9 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 const camera = new THREE.PerspectiveCamera(83, sizesCanvas.width / sizesCanvas.height, 0.1, 100)
 
 if (isMobile) {
-    camera.position.x = -0.5
-    camera.position.y = -0.5
-    camera.position.z = - 4
+    camera.position.x = 1
+    camera.position.y = 1
+    camera.position.z = -3
 } else {
     camera.position.x = 0
     camera.position.y = 0
